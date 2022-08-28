736a024acf61252a14cb10c765b0036a9133568e
nagarajtolearn <nagarajtolearn@gmail.com>
2022-08-28 21:42:53 +0530

    first commit

---
+++ src/components/movieList/MovieList.js
@@ -0,0 +1,42 @@
+import React, { useState, useEffect } from "react";
+import "./MovieList.css";
+
+import Card from "../card/Card";
+import { useParams } from "react-router-dom";
+
+const MovieList = () => {
+  const [movieList, setMovieList] = useState([]);
+  const { type } = useParams();
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  useEffect(() => {
+    getData();
+  }, [type]);
+
+  const getData = () => {
+    fetch(
+      `https://api.themoviedb.org/3/movie/${
+        type ? type : "popular"
+      }?api_key=4be6ef9ebfa768b47ed2cbb7d6941a98&language=en-US`
+    )
+      .then((res) => res.json())
+      .then((data) => setMovieList(data.results))
+      .catch((error) => console.log(error));
+  };
+
+  return (
+    <div className="movie__list">
+      <h2 className="list__title">{(type ? type : "POPOLAR").toUpperCase()}</h2>
+      <div className="list__cards">
+        {movieList.map((movie) => (
+          <Card movie={movie} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default MovieList;
---
+++ src/pages/home/Home.js
@@ -0,0 +1,67 @@
+import React, { useEffect, useState } from "react";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from "react-responsive-carousel";
+import { Link } from "react-router-dom";
+import "./Home.css";
+import MovieList from "../../components/movieList/MovieList";
+
+const Home = () => {
+  const [popularMovies, setPopularMovies] = useState([]);
+
+  useEffect(() => {
+    fetch(
+      "https://api.themoviedb.org/3/movie/popular?api_key=4be6ef9ebfa768b47ed2cbb7d6941a98&language=en-US"
+    )
+      .then((res) => res.json())
+      .then((data) => setPopularMovies(data.results))
+      .catch((error) => console.log(error));
+  }, []);
+
+  return (
+    <>
+      <div className="poster">
+        <Carousel
+          autoPlay={true}
+          infiniteLoop={true}
+          showThumbs={false}
+          transitionTime={3}
+          showStatus={false}
+        >
+          {popularMovies.map((movie) => (
+            <Link
+              style={{ textDecoration: "none", color: "white" }}
+              to={`/movie/${movie.id}`}
+              key={movie.id}
+            >
+              <div className="posterImage">
+                <img
+                  src={`https://image.tmdb.org/t/p/original${
+                    movie && movie.backdrop_path
+                  }`}
+                />
+              </div>
+              <div className="posterImage__overlay">
+                <div className="posterImage__title">
+                  {movie ? movie.original_title : ""}
+                </div>
+                <div className="posterImage__runtime">
+                  {movie ? movie.release_date : ""}
+                  <span className="posterImage__rating">
+                    {movie ? movie.vote_average : ""}
+                    <i className="fas fa-star" />
+                  </span>
+                </div>
+                <div className="posterImage__description">
+                  {movie ? movie.overview : ""}
+                </div>
+              </div>
+            </Link>
+          ))}
+        </Carousel>
+        <MovieList />
+      </div>
+    </>
+  );
+};
+
+export default Home;
